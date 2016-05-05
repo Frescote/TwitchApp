55e27049897dd90e4e2441ff3d9993054401a2ea
Frescote <totalvarez@gmail.com>
2016-05-05 15:09:50 +0100

    Color coding added

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -8,6 +8,7 @@ myApp.controller('mainController',['$scope', '$q','twitch', function($scope, $q,
 		this.logo = "assets/twitchDefaultAvatar.png";
 		this.statusBit = 1; //Offline
 		this.show = true;
+		this.class = "user-offline";
 	}
 
 	// Store user data in users array as factory delivers
@@ -19,6 +20,7 @@ myApp.controller('mainController',['$scope', '$q','twitch', function($scope, $q,
 			});
 			$scope.users[index].status = "Channel not found.";
 			$scope.users[index].statusBit = 0; //Not found
+			$scope.users[index].class = "user-not-found";
 		}else{
 			index = $scope.users.findIndex(function(element){
 				return data[0].name === element.name;
@@ -26,9 +28,10 @@ myApp.controller('mainController',['$scope', '$q','twitch', function($scope, $q,
 			$scope.users[index].name = data[0].display_name;
 			$scope.users[index].logo = data[0].logo;
 			$scope.users[index].channelUrl = data[0].url;
-			$scope.users[index].statusBit = 2; //Online
 			if(data[1].stream !== null){
 				$scope.users[index].status = data[0].status;
+				$scope.users[index].class = "user-online";
+				$scope.users[index].statusBit = 2; //Online
 			}
 		}
 		sortUserDataByStatus();
@@ -48,6 +51,11 @@ myApp.controller('mainController',['$scope', '$q','twitch', function($scope, $q,
 	// For progress bar
 	$scope.getLoadingPercentage = function()  {
 		return ((usersLoaded/$scope.users.length)*100).toString();
+	}
+
+	// Dynamic update of user class
+	$scope.getUserClass = function(user) {
+		return user.class;
 	}
 
 	//Variables
